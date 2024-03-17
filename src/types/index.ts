7c52fa9c97d8ce91b81e2f7d4ab0a3d4e82379bd
ko-karol <karol.olechno1@gmail.com>
2024-03-17 19:24:41 +0000

    chore: imported types into single index for easier exports

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+import DotStorageDefinition from './DotStorageDefinition.ts';
+import FuncArray from './FuncArray.ts';
+import KeyDefinition from './KeyDefinition.ts';
+import NamespaceDefinition from './NamespaceDefinition.ts';
+import NamespaceManagerDefinition from './NamespaceManagerDefinition.ts';
+import StrArray from './StrArray.ts';
+import StrSet from './StrSet.ts';
+
+export {
+	DotStorageDefinition,
+	FuncArray,
+	KeyDefinition,
+	NamespaceDefinition,
+	NamespaceManagerDefinition,
+	StrArray,
+	StrSet,
+};
